aaedd802c7dc8934bcd661cb3a67c3f69d21e613
Aidaho <aidaho@roxy-wi.org>
2024-05-13 12:09:15 +0300

    v1.0.4: Add 'disabled' attribute to input macro and update user roles
    The 'disabled' parameter is introduced to the input macro in this commit. It adds enhancement to user roles management by providing a selection of user roles. Several minor user interface modifications have been made and a typo in roles names defined in users.js file is corrected.

--- app/routes/admin/routes.py
+++ app/routes/admin/routes.py
@@ -56,6 +56,7 @@ def admin():
         'ldap_enable': sql.get_setting('ldap_enable'),
         'guide_me': 1,
         'user_subscription': roxywi_common.return_user_subscription(),
+        'users_roles': user_sql.select_users_roles(),
         'user_roles': user_sql.select_user_roles_by_group(user_group),
     }
 
