7aaf3a8a31204c85a86998d034dec9bdf1613f0f
Aidaho <aidaho@roxy-wi.org>
2024-07-28 11:12:47 +0300

    v1.1: Remove unused imports and deprecated routes
    This commit removes unnecessary 'json' and 'time' imports, as well as deprecated routes and functions from 'routes.py'. The cleaned code enhances maintainability by eliminating commented-out and unused portions, improving readability and reducing potential confusion.

--- app/routes/server/routes.py
+++ app/routes/server/routes.py
@@ -1,7 +1,4 @@
-import json
-import time
-
-from flask import Response, stream_with_context, jsonify
+from flask import jsonify
 from flask_jwt_extended import jwt_required
 
 from app.routes.server import bp
@@ -34,34 +31,6 @@ def check_ssh(server_ip):
         return str(e)
 
 
-# @bp.route('/check/server/<int:server_id>')
-# def check_server(server_id):
-#     def get_check():
-#         while True:
-#             try:
-#                 server = server_sql.get_server(server_id)
-#             except Exception as e:
-#                 raise e
-#             result = server_mod.server_is_up(server.ip)
-#             status = {
-#                 "status": result,
-#                 'name': server.hostname.replace("'", ""),
-#                 'desc': server.desc.replace("'", ""),
-#                 'ip': server.ip,
-#                 'port': server.port,
-#                 'enabled': server.enabled,
-#                 'creds_id': server.creds_id,
-#                 'group_id': server.group_id
-#             }
-#             yield f'data:{json.dumps(status)}\n\n'
-#             time.sleep(60)
-#
-#     response = Response(stream_with_context(get_check()), mimetype="text/event-stream")
-#     response.headers["Cache-Control"] = "no-cache"
-#     response.headers["X-Accel-Buffering"] = "no"
-#     return response@bp.route('/check/server/<int:server_id>')
-
-
 @bp.route('/check/server/<int:server_id>')
 def check_server(server_id):
     try:
@@ -80,15 +49,6 @@ def check_server(server_id):
         'group_id': server.group_id
     }
     return jsonify(status)
-
-
-@bp.route('/show/if/<server_ip>')
-def show_if(server_ip):
-    roxywi_auth.page_for_admin(level=2)
-    server_ip = common.is_ip_or_dns(server_ip)
-    command = "sudo ip link|grep 'UP' |grep -v 'lo'| awk '{print $2}' |awk -F':' '{print $1}'"
-
-    return server_mod.ssh_command(server_ip, command)
 
 
 @bp.app_template_filter('string_to_dict')
