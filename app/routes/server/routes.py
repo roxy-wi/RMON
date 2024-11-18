caadd76e750ed6aa2ae6234face7ab533a3daeaf
Aidaho <aidaho@roxy-wi.org>
2024-11-18 15:02:45 +0300

    v1.2.2: Update Debian install tasks and enhance IP handling in routes
    Refactor Ansible tasks to differentiate between new and old Debian systems. Enhance route functions to accept both IPv4/IPv6 addresses and domain names, ensuring better flexibility and compatibility.

--- app/routes/server/routes.py
+++ app/routes/server/routes.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from flask import jsonify
 from flask_jwt_extended import jwt_required
 from flask_pydantic import validate
@@ -5,11 +7,11 @@ from pydantic import IPvAnyAddress
 
 from app.routes.server import bp
 import app.modules.db.server as server_sql
-import app.modules.common.common as common
 import app.modules.roxywi.auth as roxywi_auth
 import app.modules.server.server as server_mod
 from app.views.server.views import ServerView
 from app.views.server.cred_views import CredView
+from app.modules.roxywi.class_models import DomainName
 
 
 @bp.before_request
@@ -25,7 +27,7 @@ bp.add_url_rule('/cred', view_func=CredView.as_view('cred'), methods=['POST'])
 
 @bp.route('/check/ssh/<server_ip>')
 @validate()
-def check_ssh(server_ip: IPvAnyAddress):
+def check_ssh(server_ip: Union[IPvAnyAddress, DomainName]):
     roxywi_auth.page_for_admin(level=2)
 
     try:
@@ -62,19 +64,19 @@ def string_to_dict(dict_string) -> dict:
 
 @bp.route('/system_info/get/<server_ip>/<int:server_id>')
 @validate()
-def get_system_info(server_ip: IPvAnyAddress, server_id: int):
+def get_system_info(server_ip: Union[IPvAnyAddress, DomainName], server_id: int):
     return server_mod.show_system_info(str(server_ip), server_id)
 
 
 @bp.route('/system_info/update/<server_ip>/<int:server_id>')
 @validate()
-def update_system_info(server_ip: IPvAnyAddress, server_id: int):
+def update_system_info(server_ip: Union[IPvAnyAddress, DomainName], server_id: int):
     return server_mod.update_system_info(str(server_ip), server_id)
 
 
 @bp.route('/firewall/<server_ip>')
 @validate()
-def show_firewall(server_ip: IPvAnyAddress):
+def show_firewall(server_ip: Union[IPvAnyAddress, DomainName]):
     roxywi_auth.page_for_admin(level=2)
 
     return server_mod.show_firewalld_rules(str(server_ip))
