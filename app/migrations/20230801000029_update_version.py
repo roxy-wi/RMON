15277a09b5bb99db639d1bdf2b6affefb766bc9c
Aidaho <aidaho@roxy-wi.org>
2025-05-17 15:20:52 +0300

    v1.2.15: Add conditionals and error handling for pip install steps
    Introduced logic to handle errors during pip upgrades and Python package installations across Debian and RedHat systems. Conditional checks ensure fallback to alternate methods when primary steps fail, improving reliability and compatibility.

---
+++ app/migrations/20230801000029_update_version.py
@@ -0,0 +1,22 @@
+from app.modules.db.db_model import connect, Version
+
+# Get the migrator for the current database
+migrator = connect(get_migrator=True)
+
+
+def upgrade():
+    # Insert version record with version '1.0'
+    try:
+        Version.insert(version='1.2.15').execute()
+        print("Inserted version record with version '1.2.15'")
+    except Exception as e:
+        print(f"Error inserting version record: {e}")
+
+
+def downgrade():
+    # Remove version record with version '1.0'
+    try:
+        Version.delete().where(Version.version == '1.2.14').execute()
+        print("Removed version record with version '1.2.14'")
+    except Exception as e:
+        print(f"Error removing version record: {e}")
